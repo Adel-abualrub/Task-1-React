53979c2df4aaa5f3f7f0694e952082f344bae047
Adel-abualrub <adelbassam5201@gmail.com>
2026-01-17 12:46:20 +0200

    Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './hero/Hero'
 import Portfolio from './Portfolio/Portfolio'
 import About from './About/About'
 import Contact from './Contact/Contact'
+import Footer from './footer/Footer'
 
 export default function App() {
   return <>
@@ -12,6 +13,7 @@ export default function App() {
     <Portfolio/>
     <About/>
     <Contact/>
+    <Footer/>
     
   </>
 }
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -4,7 +4,40 @@ import style from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-    
+      <footer className={`${style.footer}`}>
+        <div className={`container`}>
+          <div className={`row text-center`}>
+
+            <div className={`col-md-4`}>
+              <h4 className={`${style.title}`}>LOCATION</h4>
+              <p className={`${style.text}`}>
+                2215 John Daniel Drive <br />
+                Clark, MO 65243
+              </p>
+            </div>
+
+            <div className={`col-md-4`}>
+              <h4 className={`${style.title}`}>AROUND THE WEB</h4>
+              <p className={`${style.text}`}>
+                {/* بدون أيقونات مثل ما طلبت */}
+              </p>
+            </div>
+
+            <div className={`col-md-4`}>
+              <h4 className={`${style.title}`}>ABOUT FREELANCER</h4>
+              <p className={`${style.text}`}>
+                Freelancer is a free to use, MIT licensed Bootstrap theme
+                created by <span className={`${style.link}`}>Start Bootstrap</span>.
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </footer>
+
+      <div className={`${style.copyRight}`}>
+        <p>Copyright © Your Website 2024</p>
+      </div>
     </>
   )
 }
