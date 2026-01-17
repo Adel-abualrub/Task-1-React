6de91f4d125e700e2fba80b7a9b9d0aded7a26ea
Adel-abualrub <adelbassam5201@gmail.com>
2026-01-17 11:59:40 +0200

    navbar

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import style from './header.module.css'
+
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+  return <>
+   <nav className={`${style.customNavbar}  navbar navbar-expand-lg `}>
+  <div className={`container`}>
+    <a className={`${style.navbarBrand} text-white fw-bold`} href="#">
+      START BOOTSTRAP
+    </a>
+
+    <button
+      className={`navbar-toggler text-white`}
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+    >
+      <span className={`navbar-toggler-icon`}></span>
+    </button>
+
+    <div className={`collapse navbar-collapse`} id="navbarNav">
+      <ul className={`navbar-nav ms-auto`}>
+        <li className={`nav-item`}>
+          <a className={`${style.navLink} text-white fw-bold `}  href="#">PORTFOLIO</a>
+        </li>
+        <li className={`nav-item`}>
+          <a className={`${style.navLink} text-white fw-bold`} href="#">ABOUT</a>
+        </li>
+        <li className={`nav-item`}>
+          <a className={`${style.navLink} text-white fw-bold`} href="#">CONTACT</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</>
+  
 }
