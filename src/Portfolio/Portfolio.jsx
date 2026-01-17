cfc7f69b427badb1f993b5a7e6028d00c83844c6
Adel-abualrub <adelbassam5201@gmail.com>
2026-01-17 12:21:00 +0200

    hero section

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import style from './Portfolio.module.css'
+import ic1 from './../assets/imgs/proto/ic1.png'
+import ic2 from './../assets/imgs/proto/ic2.png'
+import ic3 from './../assets/imgs/proto/ic3.png'
+import ic4 from './../assets/imgs/proto/ic4.png'
+import ic5 from './../assets/imgs/proto/ic5.png'
+import ic6 from './../assets/imgs/proto/ic6.png'
+
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+<></>
   )
 }
--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 import style from './hero.module.css'
+import HeroImg from './../assets/imgs/heroImg/PrImg.svg'
+
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <>
+      <section className={`${style.heroSection}`}>
+        <div className={`container text-center`}>
+
+          <img
+            src={HeroImg}
+            alt="hero"
+            className={`${style.heroImg}`}
+          />
+
+          <h1 className={`${style.heroTitle}`}>
+            START BOOTSTRAP
+          </h1>
+
+          <div className={`${style.divider}`}>
+            <span className={`${style.line}`}></span>
+            <i className={`fa-solid fa-star ${style.star}`}></i>
+            <span className={`${style.line}`}></span>
+          </div>
+
+          <p className={`${style.heroText}`}>
+            Graphic Artist - Web Designer - Illustrator
+          </p>
+
+        </div>
+      </section>
+    </>
   )
 }
