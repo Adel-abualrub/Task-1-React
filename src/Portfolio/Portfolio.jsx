3b6332549fc01412018eeff1020040c8a6505f5d
Adel-abualrub <adelbassam5201@gmail.com>
2026-01-17 12:22:35 +0200

    protofolio section

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -9,6 +9,61 @@ import ic6 from './../assets/imgs/proto/ic6.png'
 
 export default function Portfolio() {
   return (
-<></>
+    <>
+      <section className={`${style.portfolioSection}`}>
+        <div className={`container`}>
+
+          <h2 className={`${style.title} text-center`}>
+            PORTFOLIO
+          </h2>
+
+          <div className={`${style.divider}`}>
+            <span className={`${style.line}`}></span>
+            <i className={`fa-solid fa-star ${style.star}`}></i>
+            <span className={`${style.line}`}></span>
+          </div>
+
+          <div className={`row g-4`}>
+
+            <div className={`col-md-4`}>
+              <div className={`${style.item}`}>
+                <img src={ic1} className={`${style.img}`} alt="" />
+              </div>
+            </div>
+
+            <div className={`col-md-4`}>
+              <div className={`${style.item}`}>
+                <img src={ic2} className={`${style.img}`} alt="" />
+              </div>
+            </div>
+
+            <div className={`col-md-4`}>
+              <div className={`${style.item}`}>
+                <img src={ic3} className={`${style.img}`} alt="" />
+              </div>
+            </div>
+
+            <div className={`col-md-4`}>
+              <div className={`${style.item}`}>
+                <img src={ic4} className={`${style.img}`} alt="" />
+              </div>
+            </div>
+
+            <div className={`col-md-4`}>
+              <div className={`${style.item}`}>
+                <img src={ic5} className={`${style.img}`} alt="" />
+              </div>
+            </div>
+
+            <div className={`col-md-4`}>
+              <div className={`${style.item}`}>
+                <img src={ic6} className={`${style.img}`} alt="" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
